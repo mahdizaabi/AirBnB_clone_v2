7c0ae5c751852384124a437a2131c891f0bdcfce
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-10 22:05:36 +0000

    adding_file

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """instance constructor and instance instantiation"""
 
-        keyx = 'created_at'
         t = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs:
-            if keyx in kwargs:
+        if len(kwargs) > 1:
+            if 'a' not in kwargs:
                 for key, value in kwargs.items():
                     if key != "__class__":
                         if key == "created_at" or key == 'updated_at':
@@ -25,23 +24,16 @@ class BaseModel:
                         else:
                             setattr(self, key, value)
             else:
+                del kwargs['a']
                 for key, value in kwargs.items():
-                    if key != "__class__":
-                        if key == "created_at" or key == 'updated_at':
-                            setattr(self, key, datetime.strptime(value, t))
-                        else:
-                            setattr(self, key, value)
-                            self.id = str(uuid.uuid4())
-                            self.created_at = datetime.now()
-                            self.updated_at = self.created_at
-                            models.storage.new(self)
-                            models.storage.save()
+                    setattr(self, key, value)
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = self.created_at
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
 
     def __str__(self):
         """string readable representation of the instance"""
@@ -51,6 +43,8 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute with the current datetime"""
         self.updated_at = datetime.now()
+        #models.storage.save()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
