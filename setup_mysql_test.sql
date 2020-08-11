255a1d775b7f897dedb6d3eb2f8a95e9fadc3908
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-11 00:48:23 +0000

    adding_file

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- a script that prepares a MySQL server for the project:SETUP TEST
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance_schema.
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+
