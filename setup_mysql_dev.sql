b79db13725af42c4903d556224e4a500b1b0bc00
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-11 00:46:00 +0000

    adding_file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
--- prepares a MySQL server for the project:
-
 -- prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
