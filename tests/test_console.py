68f0cc70b9e29af9abce166c9f7baf7eed0d0676
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-11 21:24:54 +0000

    adding_file

--- tests/test_console.py
+++ tests/test_console.py
@@ -31,5 +31,24 @@ class TestHBNBCommand(unittest.TestCase):
         self.assertIsNotNone(HBNBCommand.do_update.__doc__)
         self.assertIsNotNone(HBNBCommand.default.__doc__)
 
+    def test_create_kwargs(self):
+        """Test create command with kwargs."""
+
+        self.HBNB = HBNBCommand()
+        with patch("sys.stdout", new=StringIO()) as f:
+            call = ('create Place city_id="0001" name="My_house" '
+                    'number_rooms=4 latitude=37.77 longitude=a')
+            self.HBNB.onecmd(call)
+            pl = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all Place")
+            output = f.getvalue()
+            self.assertIn(pl, output)
+            self.assertIn("'city_id': '0001'", output)
+            self.assertIn("'name': 'My house'", output)
+            self.assertIn("'number_rooms': 4", output)
+            self.assertIn("'latitude': 37.77", output)
+            self.assertNotIn("'longitude'", output)
+
 if __name__ == "__main__":
     unittest.main()
