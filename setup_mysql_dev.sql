1a181e9b0de6dacc04553c6e3c497a5d72a6074b
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-11 00:40:18 +0000

    adding

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,9 @@
 -- prepares a MySQL server for the project:
--- A database hbnb_dev_db
--- new user hbnb_dev
--- hbnb_dev should have all privileges on the database hbnb_dev_db
--- hbnb_dev should have SELECT privilege on the database performance_schema
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO ‘hbnb_dev’@'localhost’;
+FLUSH PRIVILEGES;
+
