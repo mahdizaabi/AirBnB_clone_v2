75731fac0e23884e9f3eecc30cdd53d3b0b863d5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-11 17:32:57 +0000

    adidng_file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,9 +47,9 @@ class FileStorage():
         if cls is not None:
             new_dic = {}
             for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
+                if key.split(".")[0] == cls:
                     new_dic[key] = value
-            return new_dic
+                    return new_dic
         return self.__objects
 
     def new(self, obj):
