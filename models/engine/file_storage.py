d7251e7fa1e80bba1ece6e328cdbc3b19a624e0e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-11 17:54:32 +0000

    adding_file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,7 +50,7 @@ class FileStorage():
             for key, value in self.__objects.items():
                 if cls == value.__class__ or cls == value.__class__.__name__:
                     new_dic[key] = value
-                return new_dic
+                    return new_dic
         return self.__objects
 
     def new(self, obj):
