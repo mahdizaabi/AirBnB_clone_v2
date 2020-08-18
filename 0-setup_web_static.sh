ef3b43df647431b22713e5a89bdb40911afbd72b
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-18 02:42:29 +0000

    adidng_file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#deploy web_static for web server
+apt -y update
+apt install -y nginx
+mkdir -pv /data/web_static/releases/test/
+mkdir -pv /data/web_static/shared/
+echo "Test Static deployement" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+echo "
+server {
+  listen 80;
+  listen [::]:80 default_server;
+  location /hbnb_static {
+    alias /data/web_static/current/;
+  }
+  index index.html;
+}" > /etc/nginx/sites-available/default
+service nginx restart
