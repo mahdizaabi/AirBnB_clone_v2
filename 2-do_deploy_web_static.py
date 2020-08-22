b965fbd7acda2a6b7391080c60157715d3310713
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-22 00:42:32 +0000

    adding_file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,13 +18,13 @@ def do_deploy(archive_path):
         absname = fname.split(".")[:-4]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, absname))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(fname, path, absname))
-        run('rm /tmp/{}'.format(fname))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, absname))
-        run('rm -rf {}{}/web_static'.format(path, absname))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, absname))
+        run('sudo mkdir -p {}{}/'.format(path, absname))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(fname, path, absname))
+        run('sudo rm /tmp/{}'.format(fname))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, absname))
+        run('sudo rm -rf {}{}/web_static'.format(path, absname))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, absname))
         return True
     except:
         return False
