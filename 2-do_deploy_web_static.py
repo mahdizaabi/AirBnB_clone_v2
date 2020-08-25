1012e7ac23e506bbb8e64f90555f6806e4fb267c
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-25 22:16:40 +0000

    adding_file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,25 +9,24 @@ env.hosts = ['35.185.121.162', '54.167.76.210']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-
-    filename = archive_path.split("/")[-1].split(".")[0]
-    path = "/data/web_static/releases/{}/web_static/*".format(filename)
+    """Distribute an Archive to servers"""
+    if not os.path.exists(archive_path):
+        return false
     try:
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
-            format(filename, filename))
-        run("sudo rm /tmp/{}.tgz".format(filename))
-        run("sudo mv {} /data/web_static/releases/{}/".format(path, filename))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".
-            format(filename))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(filename))
+        file_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p /data/web_static/releases/{}'.format(file_name))
+        run('sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.
+            format(file_name, file_name))
+        run('sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.
+            format(file_name, file_name))
+        run('sudo rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+        run('sudo rm -rf /tmp/{}.tgz'.format(file_name))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -sf /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(file_name))
         return True
     except:
         return False
-    return True
