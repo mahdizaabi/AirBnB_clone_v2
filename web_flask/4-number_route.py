bcf18040f1fc9fa72d083831f8df617440470f7e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-01 15:47:36 +0000

    adding_file

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """return hello hbnb"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hellox():
+    """return html content"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hex(text):
+    """return html content"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def python(text):
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def check_for_integer(n):
+    if type(n) is int:
+        return '%d' % n
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
