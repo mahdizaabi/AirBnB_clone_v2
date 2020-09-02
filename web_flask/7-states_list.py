17868c2c7f867cfa7b3397bce57236ea57a30411
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-02 14:46:18 +0000

    add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,24 +2,25 @@
 """
 web application
 """
-from flask import Flask, render_template
 from models import storage
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def view_states():
-    """ display list states"""
-    data = storage.all("State")
-    return render_template("7-states_list.html", data=data)
-
-
 @app.teardown_appcontext
-def app_context(fake):
-    """teardown_appcontext"""
+def teardown(error):
+    """teardown
+    """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+@app.route('/states_list', strict_slashes=False)
+def statesList():
+    """liste state:
+    """
+    data = storage.all("State")
+    return render_template('7-states_list.html', data=data)
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
